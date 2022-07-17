19c8e4f09a791a71764ea0bd679fdb6112887c68
SGolomzin <s.golomzin@gmail.com>
2022-07-18 00:42:51 +0300

    Add hamberger menu for mobile view

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function calcDiffPoly(percent, part) {
 
 	return `polygon(${a}% 0%, ${b}% 100%, ${c}% 100%, ${d}% 0%)`
 }
+
+const btnMobileMainHeaderMenu = document.querySelector(".main-header__toggle-menu-btn");
+const mainHeaderMenu = document.querySelector(".main-menu");
+
+btnMobileMainHeaderMenu.addEventListener("click", (e) => {
+	e.target.classList.toggle("main-header__toggle-menu-btn--open");
+	e.target.classList.toggle("main-header__toggle-menu-btn--close");
+	var btnValue = e.target.querySelector("span");
+
+	var isOpen = +btnValue.dataset.open;
+
+	if (isOpen) {
+		btnValue.innerText = "Открыть меню";
+		btnValue.dataset.open = "0";
+		mainHeaderMenu.style.display = "none";
+	} else {
+		btnValue.innerText = "Закртыть меню";
+		btnValue.dataset.open = "1";
+		mainHeaderMenu.style.display = "block";
+	}
+})
