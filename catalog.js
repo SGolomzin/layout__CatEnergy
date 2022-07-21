aab6be7495c3153f43662061772ebd63720d01c1
SGolomzin <s.golomzin@gmail.com>
2022-07-21 17:02:10 +0300

    Separate scripts for each page. Add products section.

---
+++ catalog.js
@@ -0,0 +1,62 @@
+const productsList = [
+	{
+		"title": "Cat Energy PRO 500 г",
+		"info": {
+			"Масса": "500 г",
+			"Вкус": "Курица",
+			"Цена": "700 Р"
+		}
+	},
+	{
+		"title": "Cat Energy PRO 1000 г",
+		"info": {
+			"Масса": "1000 г",
+			"Вкус": "Курица",
+			"Цена": "1000 Р"
+		}
+	},
+	{
+		"title": "Cat Energy PRO 500 г",
+		"info": {
+			"Масса": "500 г",
+			"Вкус": "Рыба",
+			"Цена": "700 Р"
+		}
+	},
+	{
+		"title": "Cat Energy PRO 1000 г",
+		"info": {
+			"Масса": "1000 г",
+			"Вкус": "Рыба",
+			"Цена": "1000 Р"
+		}
+	}
+];
+
+
+let cardTemplate = document.getElementById("product-card-template");
+let productsContainer = document.querySelector(".products__catalog");
+
+productsList.forEach(product => {
+	let card = cardTemplate.content.cloneNode(true);
+	let title = card.querySelector(".product-card__title");
+	let tableBody = card.querySelector(".info-table__body");
+
+	title.textContent = product.title;
+
+	for (let [key, value] of Object.entries(product.info)) {
+		let tr = document.createElement("tr");
+		let tdKey = document.createElement("td");
+		let tdValue = document.createElement("td");
+
+		tdKey.textContent = key;
+		tdValue.textContent = value;
+
+		tr.append(tdKey, tdValue);
+		tr.classList.add("info-table__row");
+
+		tableBody.append(tr);
+	}
+
+	productsContainer.append(card);
+})
--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const diffImgBefore = document.querySelector(".before-after__img-before");
-const diffImgAfter = document.querySelector(".before-after__img-after");
-
-function transformDiff(percent) {
-	diffImgAfter.style.clipPath = calcDiffPoly(percent, 'l');
-	diffImgBefore.style.clipPath = calcDiffPoly(percent, 'r');
-}
-
-function calcDiffPoly(percent, part) {
-	let a = b = 0, c = d = 100;
-
-	if (part === 'l') {
-		a = b = Math.round(percent * 100);
-	} else if (part === 'r') {
-		c = d = Math.round(percent * 100);
-	}
-
-	return `polygon(${a}% 0%, ${b}% 100%, ${c}% 100%, ${d}% 0%)`
-}
-
 const btnMobileMainHeaderMenu = document.querySelector(".main-header__toggle-menu-btn");
 const mainHeaderMenu = document.querySelector(".main-menu");
 
