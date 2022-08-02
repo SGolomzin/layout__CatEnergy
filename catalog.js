486ff33ab4a4a3149611757faaf893aa72cf0802
SGolomzin <s.golomzin@gmail.com>
2022-08-02 23:55:38 +0300

    Add thumbnail images for catalog page

--- catalog.js
+++ catalog.js
@@ -5,6 +5,12 @@ const productsList = [
 			"Масса": "500 г",
 			"Вкус": "Курица",
 			"Цена": "700 Р."
+		},
+		"image": {
+			"src": "./public/catalog/energy_pro_500_chicken_desktop@1x.png",
+			"srcset": "./public/catalog/energy_pro_500_chicken_desktop@2x.png 2x",
+			"alt": "Изображение упаковки со вкусом курицы объемом 500 грамм",
+			"loading": "lazy"
 		}
 	},
 	{
@@ -13,6 +19,12 @@ const productsList = [
 			"Масса": "1000 г",
 			"Вкус": "Курица",
 			"Цена": "1000 Р."
+		},
+		"image": {
+			"src": "./public/catalog/energy_pro_1000_chicken_desktop@1x.png",
+			"srcset": "./public/catalog/energy_pro_1000_chicken_desktop@2x.png 2x",
+			"alt": "Изображение упаковки со вкусом курицы объемом 1000 грамм",
+			"loading": "lazy"
 		}
 	},
 	{
@@ -21,6 +33,12 @@ const productsList = [
 			"Масса": "500 г",
 			"Вкус": "Рыба",
 			"Цена": "700 Р."
+		},
+		"image": {
+			"src": "./public/catalog/energy_pro_500_fish_desktop@1x.png",
+			"srcset": "./public/catalog/energy_pro_500_fish_desktop@2x.png 2x",
+			"alt": "Изображение упаковки со вкусом рыбы объемом 500 грамм",
+			"loading": "lazy"
 		}
 	},
 	{
@@ -29,6 +47,12 @@ const productsList = [
 			"Масса": "1000 г",
 			"Вкус": "Рыба",
 			"Цена": "1000 Р."
+		},
+		"image": {
+			"src": "./public/catalog/energy_pro_1000_fish_desktop@1x.png",
+			"srcset": "./public/catalog/energy_pro_1000_fish_desktop@2x.png 2x",
+			"alt": "Изображение упаковки со вкусом рыбы объемом 1000 грамм",
+			"loading": "lazy"
 		}
 	},
 	{
@@ -37,6 +61,12 @@ const productsList = [
 			"Масса": "500 г",
 			"Вкус": "Гречка",
 			"Цена": "400 Р."
+		},
+		"image": {
+			"src": "./public/catalog/energy_pro_500_buckwheat_desktop@1x.png",
+			"srcset": "./public/catalog/energy_pro_500_buckwheat_desktop@2x.png 2x",
+			"alt": "Изображение упаковки со вкусом гречки объемом 500 грамм",
+			"loading": "lazy"
 		}
 	},
 	{
@@ -45,6 +75,12 @@ const productsList = [
 			"Масса": "1000 г",
 			"Вкус": "Гречка",
 			"Цена": "700 Р."
+		},
+		"image": {
+			"src": "./public/catalog/energy_pro_1000_buckwheat_desktop@1x.png",
+			"srcset": "./public/catalog/energy_pro_1000_buckwheat_desktop@2x.png 2x",
+			"alt": "Изображение упаковки со вкусом гречки объемом 1000 грамм",
+			"loading": "lazy"
 		}
 	},
 	{
@@ -53,6 +89,12 @@ const productsList = [
 			"Масса": "500 г",
 			"Вкус": "Рис",
 			"Цена": "500 Р."
+		},
+		"image": {
+			"src": "./public/catalog/energy_pro_500_rice_desktop@1x.png",
+			"srcset": "./public/catalog/energy_pro_500_rice_desktop@2x.png 2x",
+			"alt": "Изображение упаковки со вкусом риса объемом 500 грамм",
+			"loading": "lazy"
 		}
 	}
 ];
@@ -60,10 +102,13 @@ const productsList = [
 let cardTemplate = document.getElementById("product-card-template");
 let productsContainer = document.querySelector(".products__catalog");
 
+let productList = [];
 productsList.forEach(product => {
 	let card = cardTemplate.content.cloneNode(true);
 	let title = card.querySelector(".product-card__title");
 	let tableBody = card.querySelector(".info__table-body");
+	let thumbnail = card.querySelector(".product-card__thumbnail");
+	let image = document.createElement("img");
 
 	title.textContent = product.title;
 
@@ -81,5 +126,13 @@ productsList.forEach(product => {
 		tableBody.append(tr);
 	}
 
-	productsContainer.prepend(card);
+	for (let [key, value] of Object.entries(product.image)) {
+		image.setAttribute(key, value);
+	}
+	image.classList.add("product-card__thumbnail-image");
+	thumbnail.append(image);
+
+	productList.push(card);
 })
+
+productsContainer.prepend(...productList)
